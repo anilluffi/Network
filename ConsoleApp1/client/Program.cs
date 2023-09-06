aba5acc6327ee8ce062519f4b414f4b1c15204be
Veronica <veronikan322@gmail.com>
2023-09-06 17:36:52 +0300

    .

---
+++ ConsoleApp1/client/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string serverIp = "127.0.0.1";   // localhost
+const int serverPort = 8080;
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+try
+{
+    while(true)
+    {
+        clientF();
+
+
+    }
+    
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+
+}
+
+Console.ReadLine();
+
+
+
+void clientF()
+{
+    Console.Write("Do you want to know the time? (yes/no) ");
+    string? message = Console.ReadLine();
+
+    if (message == "y" || message == "yes")
+    {
+        socket.Connect(serverEP);                       // BLOCKING
+        socket.Send(Encoding.UTF8.GetBytes(message));   // BLOCKING
+
+        byte[] buffer = new byte[256];
+        int byteCount = 0;
+        string response = string.Empty;
+
+        do
+        {
+            byteCount = socket.Receive(buffer);               // BLOCKING (Ожидание данных в сокете для чтения)
+            response += Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+        } while (socket.Available > 0);
+
+        Console.WriteLine($"RESPONSE: {response}");
+
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+    else if (message == "n" || message == "no")
+    {
+        Console.Write("Are you sure you don't want to know the time? (yes/no) ");
+        message = Console.ReadLine();
+        if (message == "y" || message == "yes")
+        { 
+            clientF(); 
+        }
+        else if (message == "n" || message == "no")
+        {
+            Console.WriteLine("You need to think carefully about whether or not you want to see the time. I'll give you time to think");
+            Thread.Sleep(10000);
+            clientF();
+        }
+        else { Console.WriteLine("Incorrect input"); }
+    }
+    else { Console.WriteLine("Incorrect input"); }
+}
---
+++ ConsoleApp1/server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string serverIp = "127.0.0.1";   // localhost
+const int port = 8080;
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(serverIp), port);
+
+try
+{
+    socket.Bind(endPoint);
+    socket.Listen(10);
+
+    Console.WriteLine($"Server started at {serverIp}:{port}");
+
+    await RunListenAsync();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
+
+async Task RunListenAsync()
+{
+    while (true)
+    {
+        Socket remoteSocket = await socket.AcceptAsync();
+
+        if (remoteSocket.RemoteEndPoint is IPEndPoint remoteEP)
+        {
+            Console.WriteLine($"Connection opened for remote --> {remoteEP.Address}:{remoteEP.Port}");
+        }
+
+        _ = Task.Run(() => HandleRequest(remoteSocket));
+    }
+}
+
+void HandleRequest(Socket remoteSocket)
+{
+    byte[] buffer = new byte[256];
+    int byteCount = 0;
+    string message = string.Empty;
+    DateTime currentTime = DateTime.Now;
+    do
+    {
+        byteCount = remoteSocket.Receive(buffer);               // BLOCKING (Ожидание данных в сокете для чтения)
+        message += Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+    } while (remoteSocket.Available > 0);
+
+    Console.WriteLine($"{DateTime.Now.ToShortTimeString()} > {message}");
+
+    //Thread.Sleep(10000);
+
+    string response = "Текущее время: " + currentTime.ToString("HH:mm:ss");
+    remoteSocket.Send(Encoding.UTF8.GetBytes(response));
+
+    remoteSocket.Shutdown(SocketShutdown.Both);
+    remoteSocket.Close();
+    Console.WriteLine("Connection closed\n");
+}
